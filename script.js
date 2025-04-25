6dcb116fee99294a9cef029c6830e844255a71d5
Ng Kah Lok <ngkahlok0407@gmail.com>
2025-04-26 00:54:09 +0800

    Completed

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 //JavaScript file created
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
+var sideMenu = document.getElementById("sideMenu");
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwTctpb6cm-3_k4mfQ0Mm-mwxXumKw8F1LxK4Wz6OvQ5GssZaQcT9MVW_4SANjKdyk_/exec';
+const form = document.forms['submit-to-google-sheet'];
+const msg = document.getElementById("msg");
+
+form.addEventListener('submit', e => {
+ e.preventDefault()
+ fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+   .then(response => {
+        msg.innerHTML = "Messsage sent successfully";
+        setTimeout(function(){
+        msg.innerHTML = ""
+        }, 5000)
+        form.reset();
+    })
+   .catch(error => console.error('Error!', error.message))
+})
 
 function opentab(tabName){
     for(tablink of tablinks){
@@ -13,5 +31,13 @@ function opentab(tabName){
 
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabName).classList.add("active-tab");
-
 }
+
+function closeMenu(){
+    sideMenu.style.right = "-200px";
+}
+
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+ 

1102572dc5c6a35c3a2a4d2103a351ab87bf2bb2
Ng Kah Lok <ngkahlok0407@gmail.com>
2025-04-26 00:15:04 +0800

    Completed desktop version

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 //JavaScript file created
-function load(){
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabName){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabName).classList.add("active-tab");
 
 }
